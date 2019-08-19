ffc29ab97551354ca8a0076a9cdf38d16318f986
cu3l3bre <cu3l3bre@googlemail.com>
2019-08-19 11:58:50 +0200

    first one

---
+++ HalloCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // My first c# program
+            Console.WriteLine("Hello C# World");
+            Console.Write("Hello C# World \n");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("Hello C# World");
+            
+            Console.ReadKey();
+        }
+    }
+}
